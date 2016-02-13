17fde9a140e2daeb035e357cece5c127e91c2049
Flub78 <frederic.peignot@free.fr>
2016-02-13 10:57:39 +0100

    Première version basée sur citemplate avec un menu différent

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Il permet
 
 
 Evolutions par rapport à la version 2
-* Il est basé sur COdeIgniter 3.0
+* Il est basé sur CodeIgniter 3.0
 * Il utilise bootstrap afin d'etre utilisable sur téléphone, tablette et grands écrans
 * Il offre une meilleure couverture de test.
 * Le module d'identification qui provoquait des déconnexions intempestives a été remplacé
@@ -25,3 +25,39 @@ Liens
 Lien vers la version 2: http://projets.developpez.com/projects/gvv/wiki/Documentation_utilisateur
 
 Git repo: https://github.com/flub78/GVV3
+
+## Installation
+
+### Pré-requis
+    * Un serveur WEB, par exemple Apache
+    * Un serveur Mysql, (ou autre base de données supporté par CodeIgniter 3)
+    * PHP 5.5 ou supérieur
+    
+### Etapes
+    * Extraire le projet citemplate dans le répertoire de votre serveur WEB (/var/www/html sous Ubuntu). GVV3 peut aussi être installé en https.
+    * Extraire ce projet dnas le même répertoire. (Certain fichiers sont remplacés)
+    * Créer une base de données et un utilisateur
+    * Editer et adapter les fichiers application/config/config.php et application/config/database.php
+    
+    * Lancer le programme, suivez ses indications sur les droits des répertoires
+    * Il créera lui même 
+    
+#### Installation sur une base GVV2 active
+
+    Il est possible de faire fonctionner le programme sur une base GVV2 active. Référencez juste la base et l'utilisateur GVV2.
+    Attention des tables suplémentaries vont être crées dans la base. Elle ne seront pas sauvegardées par GVV2. La compatibilité n'est assurée qu'entre la dernière version de GVV2 et les premières version de GVV3. Les évolutions de GVV3 pour faire migrer la base de façon incompatible avec GVV3.
+    
+    C'est une configuration de dévelopement et de test
+    
+#### Installation à partir d'une sauvegarde de GVV2
+
+    Si vous copiez la denière sauvegarde de GVV2 sous install/database.sql, votre sauvegarde sera utilisée pour peupler 
+    la base de données. Attention cela ne fonctionne qu'avec la dernière version de GVV2. Si vous n'êtes pas à jour,
+    effectuez d'abord la mise à jour.    
+
+#### Mise à jour
+
+    * Sauvegardez votre base et vos fichiers de configuration
+    * Copiez juste la nouvelle version du programme (d'abord citemplate, puis GVV3) dans le répertoire de votre serveur WEB.
+    * Fusionnez vos ficheir de configuration avec la version du programme. Ou ajoutez les nouveaux paramètres s'il y en a dans vos fichiers de configuration.
+    * Relancez le programme, la migration de la base de données est automatique.
---
+++ application/views/menu.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ *    Project {$PROJECT]
+ *    Copyright (C) 2015 {$AUTHOR}
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Menu
+ * @package vues
+ *
+ * Menus are rendered hierachical list of anchors
+ * - the tree must be determine from the contex: current user, page and privileges
+ * - it must be rendered in HTML
+ * - every level must be decorated depending on the framework used for rendering (bootstrap, etc).
+ *
+ * a menu element has
+ *   a label
+ *   an url to call
+ *   a role that determine if this element is visible depending on the user
+ *   eventually a submenu, which is a list of menu elements.
+ *   a class fo the element
+ */
+
+
+
+// #################################################################################################
+$menu_bootstrap = array (
+		'label' => "Bootstrap",
+//		'class' => 'menuheader',
+		'url' => controller_url ( "bootstrap" ),
+		'submenu' => array (
+				array (
+						'label' => "Basic",
+						'url' => controller_url ( "bootstrap/basic" )
+				),
+				array (
+						'label' => "Blog",
+						'url' => controller_url ( "bootstrap/blog" )
+				),
+				array (
+						'label' => "Carousel",
+						'url' => controller_url ( "bootstrap/carousel" )
+				),
+				array (
+						'label' => "Cover",
+						'url' => controller_url ( "bootstrap/cover" )
+				),
+				array (
+						'label' => "Dashboard",
+						'url' => controller_url ( "bootstrap/dashboard" )
+				),
+				array (
+						'label' => "Grids",
+						'url' => controller_url ( "bootstrap/grids" )
+				),
+				array (
+						'label' => "jumbotron",
+						'url' => controller_url ( "bootstrap/jumbotron" )
+				),
+				array (
+						'label' => "narrow_jumbotron",
+						'url' => controller_url ( "bootstrap/narrow_jumbotron" )
+				),
+				array (
+						'label' => "sign_in",
+						'url' => controller_url ( "bootstrap/sign_in" )
+				),
+				array (
+						'label' => "sticky_footer_with_navbar",
+						'url' => controller_url ( "bootstrap/sticky_footer_with_navbar" )
+				),
+				array (
+						'label' => "Theme",
+						'url' => controller_url ( "bootstrap/theme" )
+				)
+		)
+);
+
+// #################################################################################################
+$menu_dev = array (
+		'label' => "Dev",
+		// 'class' => 'menuheader',
+		'submenu' => array (
+				array (
+						'label' => "info",
+						'url' => controller_url ( "dev/info" )
+				),
+				array (
+						'label' => "PhpInfo",
+						'url' => controller_url ( "dev/phpinfo" )
+				),
+				$menu_bootstrap,
+				array (
+						'label' => "CodeIgniter",
+						'url' => "http://localhost/citemplate/user_guide/"
+				),
+				array (
+						'label' => "Check French",
+						'url' => controller_url("dev/check_lang/french/1"),
+				        'role' => 'admin'
+				),
+		        array (
+		                'label' => "Meta test",
+		                'url' => controller_url("meta_test1"),
+		                'role' => 'admin'
+		        ),
+		        array (
+		                'label' => "Meta test 2",
+		                'url' => controller_url("meta_test2"),
+		                'role' => 'admin'
+		        ),
+		        array (
+		                'label' => "REST",
+		                'url' => controller_url("rest_server"),
+		                'role' => 'admin'
+		        ),
+		        array (
+		                'label' => "REST client",
+		                'url' => controller_url("dev/rest_client"),
+		                'role' => 'admin'
+		        ),
+		        array (
+		                'label' => "Flight logs",
+		                'url' => controller_url("flight_logs/select")
+		        ),
+
+		)
+);
+
+// #################################################################################################
+
+$menu_database = array (
+        'label' => "Database",
+        'role' => array('admin'),
+        //		'class' => 'dropdown-menu multi-level',
+        'submenu' => array (
+                array (
+                        'label' => "Backup",
+                        'url' => controller_url ( "databaseMgt/backup" )
+                ),
+				array (
+						'label' => "Backup views",
+						'url' => controller_url ( "databaseMgt/backup/views" )
+				),
+                array (
+                        'label' => "Restore",
+                        'url' => controller_url ( "databaseMgt/restore" )
+                ),
+                array (
+                        'label' => "Reset",
+                        'url' => controller_url ( "databaseMgt/reset" ),
+                        'onclick' => "return confirm('" . translation('reset_database') . "')"
+                )
+        )
+);
+
+
+$menu_admin = array (
+		'label' => "Admin",
+//		'class' => 'dropdown-menu multi-level',
+		'role' => 'admin',
+		'submenu' => array (
+				array (
+						'label' => "Config",
+						'url' => controller_url ( "admin/config" )
+				),
+				$menu_database,
+				array (
+						'label' => "Lock",
+						'url' => controller_url ( "admin/lock" )
+				),
+				array (
+						'label' => "Users",
+						'url' => controller_url ( "auth" )
+				),
+				array (
+						'label' => "Groups",
+						'url' => controller_url ( "groups/all" )
+				)
+		)
+);
+
+// #################################################################################################
+$menu_user = array (
+		'label' => "Users",
+		'submenu' => array (
+				array (
+						'label' => "Change password",
+						'url' => controller_url ( "auth/change_password" )
+				),
+		        array (
+		                'label' => "Users",
+		                'url' => controller_url ( "users" )
+		        )
+
+		)
+);
+
+$menu_help = array (
+		'label' => "Help",
+//		'class' => 'menuheader',
+		'submenu' => array (
+				array (
+						'label' => "About",
+						'url' => controller_url ( "welcome/about" )
+				)
+		)
+);
+
+$menu_members = array (
+        'label' => "Members",
+        //		'class' => 'menuheader',
+        'submenu' => array (
+                array (
+                        'label' => "List",
+                        'url' => controller_url ( "welcome/about" )
+                )
+        )
+);
+
+if (ENVIRONMENT == 'development') {
+	$menubar = array ('class' => 'menubar',
+		'submenu' => array($menu_admin, $menu_dev, $menu_members, $menu_user, $menu_help)
+	);
+} else {
+	$menubar = array ('class' => 'menubar',
+			'submenu' => array($menu_admin, $menu_members, $menu_user, $menu_help)
+	);
+}
+
+?>
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+	<div class="container-fluid">
+
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed"
+				data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+				aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?= base_url();?>"> <?= translation('app_name');?></a>
+		</div>
+
+		<div id="navbar" class="collapse navbar-collapse">
+
+			<?php echo bootstrap_menu($menubar); ?>
+
+			<ul id="main-menu" class="nav navbar-nav navbar-right">
+            <?php
+			  if ($this->ion_auth->logged_in ()) {
+				$user = $this->ion_auth->user()->row();
+				echo $user->email;
+				$username = $user->first_name . ' ' . $user->last_name;
+
+				echo "<li class=\"nav-welcome\"><h5>" . translation('login_welcome') . " " . $username . "</h5></li>";
+				echo "<li>&nbsp;</li>";
+				echo "<li><a id=\"logout\" href=\"" . controller_url("welcome/logout") ."\">" . translation('button_logout') . "</a></li>";
+ 			  } else {
+				echo "<li><a href=\"login\">" . translation('button_login') . "</a></li>";
+				if ($this->config->item('autoregister')) {
+					echo "<li><a href=\"register\">" . translation('button_register') . "</a></li>";
+				}
+			  }
+			?>
+
+            </ul>
+		</div>
+		<!--container-fluid -->
+
+	</div>
+	<!-- container-fluid -->
+</nav>
+
